f719e1ac6d25f66638d6aafca3b5d545d4c1cb36
Ann835 <annmallum@gmail.com>
2025-08-16 23:03:25 +0100

    update

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const counters = document.querySelectorAll('conter');
-const speed = 200;
- 
-counters.forEach(counter => {
-  const updateCount = () => {
-    const target = counter.getAttribute('data-target');
+// const counters = document.querySelectorAll (".counters span");
+// const container = document.querySelector("counters");
 
-     console.log(target);
-  }
-  updateCount();
-});
-let buttons = document.querySelector(".font-btn");
-let button = buttons.querySelectorAll(".btn1");
+// let activated = false;
+// window.addEventListener("scroll", () => {
+//    if( pageYOffset > container.offsetTop - container.offsetHeight -200
+//     && activated ===false
+// ){
+//     counters.forEach (counter => {
+//         counter.innerText = 0;
 
-        for(var i = 0; i < button.length; i++){
-            button[i].addEventListener('click', function(){
-                var current = document.getElementsByClassName('btn-active');
-                current[0].className = current[0].className.replace("btn-active", "");
-                this.className += " btn-active"
-            })
-        }
+//         let count = 0;
+
+//         function updateCount(){
+//             const target = parseInt(counter.dataset.count);
+
+//             if (count < target){
+//                 count ++;
+
+//                 counter.innerText = count;
+
+//                 setTimeout(updateCount, 10);
+//             } else {
+//                 counter.innerText = target;
+//             }
+//         }
+//         updateCount();
+
+//         activated = true;
+//     });elseif (pageYOffset > container.offsetTop - container.offsetHeight -500
+//         || pageYOffset === 0
+//         && activated ===true)
+//     {
+//         counters.forEach (counter => {
+//             counter.innerText = 0;
+//         });
+//         activated ===false;
+//     }
+// }
+// }); 
---
+++ scroll.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.navbar');
+ScrollReveal().reveal('.headerimg', { delay: 500 });
+ScrollReveal().reveal('.accordion', { delay: 500 });
+ScrollReveal().reveal('.container', { delay: 500 });
+ScrollReveal().reveal('.row', { delay: 500 });
